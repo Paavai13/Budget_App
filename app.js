acefba7bc87ee9e05b3c68a83545e70a628e397b
Paavai13 <paavaisen13@gmail.com>
2020-03-07 11:39:26 +0530

    get input values

--- app.js
+++ app.js
@@ -3,18 +3,36 @@ var budgetController = (function() {
 })();
 
 var UIcontroller = (function() {
-
+    var DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+    return {
+        getInput :function(){
+            return {
+                type: document.querySelector(DOMStrings.inputType).value,
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value
+            };
+        } ,
+        getDOMStrings: function(){
+            return DOMStrings;
+        }
+    }
 })();
 
 var controller = (function(budgetCtlr,UICtrl){
-
+    var DOM = UICtrl.getDOMStrings();
     var ctrlAddItem = function(){
-        console.log("it works");
+        var input = UICtrl.getInput();
+        console.log(input);
     }
 
-    document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem);
     document.addEventListener('keypress',function(){
-        if(event.keyCode === 13)
+        if(event.keyCode === 13 || event.which === 13)
             ctrlAddItem();
     });
 

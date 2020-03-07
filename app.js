208a0d900af2a8c581c123b002a9ef8a3d471cad
Paavai13 <paavaisen13@gmail.com>
2020-03-07 11:15:47 +0530

    event listeners added

--- app.js
+++ app.js
@@ -8,4 +8,14 @@ var UIcontroller = (function() {
 
 var controller = (function(budgetCtlr,UICtrl){
 
+    var ctrlAddItem = function(){
+        console.log("it works");
+    }
+
+    document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);
+    document.addEventListener('keypress',function(){
+        if(event.keyCode === 13)
+            ctrlAddItem();
+    });
+
 })(budgetController,UIcontroller);
